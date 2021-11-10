96a12cf309780ecd9e35414e606316ac0a14f283
rafaelreisramos <rrramos@gmail.com>
2021-11-09 23:59:11 -0300

    fix: add default jwt secret

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT'),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', 'e26f49b57b6a93395bd8ca2c31f4fe72'),
     },
   },
 });
