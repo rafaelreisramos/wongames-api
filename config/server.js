57187330f48f479f87f6fa97a3faaf6d3b19d72b
rafaelreisramos <rrramos@gmail.com>
2021-11-11 19:53:41 -0300

    replace localhost with 127.0.0.1

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST'),
-  port: env.int('PORT'),
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e26f49b57b6a93395bd8ca2c31f4fe72'),
