da244460a5bd8d44d9d718cbefa7250e297c4ba0
rafaelreisramos <rrramos@gmail.com>
2021-11-16 20:32:41 -0300

    build: update env variables

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', 'localhost'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e26f49b57b6a93395bd8ca2c31f4fe72'),
+      secret: env('ADMIN_JWT_SECRET', 'jwt_secret'),
     },
   },
 });
