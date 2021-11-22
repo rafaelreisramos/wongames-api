fff728563cbf50aa499e36220ede3ad7f97dde23
rafaelreisramos <rrramos@gmail.com>
2021-11-22 18:51:44 -0300

    refactor: add environment/default values

--- config/database.js
+++ config/database.js
@@ -4,13 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL'),
+        client: env('DATABASE_CLIENT', 'postgres'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'wongames'),
+        username: env('DATABASE_USERNAME', 'wongames'),
+        password: env('DATABASE_PASSWORD', 'wongames123'),
+        ssl: env.bool('DATABASE_SSL', false) ? {
+          require: true,
+          rejectUnauthorized: false,
+        } : false,
       },
       options: {}
     },
